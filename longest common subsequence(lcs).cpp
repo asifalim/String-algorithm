5ed2deef4dec701ccfc9ea3eed834647b6c2b52c
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2020-11-19 09:46:12 +0600

    Update longest common subsequence(lcs).cpp

--- longest common subsequence(lcs).cpp
+++ longest common subsequence(lcs).cpp
@@ -5,7 +5,7 @@ int main()
 {
     int t,i,j;
     int b,c,d,e,ans=0;
-    string s,ss;
+    string s,ss,st2;
     while(cin>>s>>ss)
     {
         d=s.size();
@@ -14,17 +14,18 @@ int main()
         {
             for(int j=0; j<e; j++)   //large length
             {
-                if(s[i]==ss[j])
-                {
-                    dp[i+1][j+1] = dp[i][j] + 1;
-                }
-                else
-                {
-                    dp[i+1][j+1] = max(dp[i][j+1],dp[i+1][j]);
-                }
+                if(s[i]==ss[j])dp[i+1][j+1] = dp[i][j] + 1;
+                else dp[i+1][j+1] = max(dp[i][j+1],dp[i+1][j]);
             }
         }
         cout<<dp[d][e]<<endl;
+        //for printing lcs
+        i=d;j=e;
+        while(i>0 and j>0)
+        {
+        if(s[i-1]==ss[j-1])st2=s[i-1]+st2,i--,j--;
+        else if(dp[i-1][j]>dp[i][j-1])i--;else j--;
+        }
     }
     return 0;
 }
